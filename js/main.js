ebe5cef16c55078cbd3b4f984bc33080809b0cc9
bigeppo <carlog91cast@gmail.com>
2022-06-17 16:46:21 +0200

    mail: add if

--- js/main.js
+++ js/main.js
@@ -11,13 +11,20 @@ const userMail = prompt('enter your email address');
 
 // 3 FURI DAL FOR, PONGO L CONDIZIONE FALSE PER SELEZIONARE LE MAIL CHE L'UTENTE ANDRà AD INSERIRE PER TROVARLA NELL'ELENCO
 
-let mailchoosen = false;
+let mailChoosen = false;
 
 // creo un ciclo for per implementare la ricerca della mail inserita dall'utente all'interno dell'array creato usando la logica di Boole
 
  for (let index = 0; index < mailList.length; index++) {
     console.log(index);
 
-    
+    // ora stabilisco un'identità fra l'elemento cercato dall'utente e l'elemento presente nell'array
+
+    if( userMail === mailList ){
+        mailChoosen = true;
+    }
 
 }
+
+console.log(mailChoosen);
+
